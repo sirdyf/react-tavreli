72e83434274e0b9b53535060ad37b1b1f630e2ec
Dmitry7456 <sir.dyf@gmail.com>
2018-12-30 14:34:07 +0500

    use express static

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/test', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
