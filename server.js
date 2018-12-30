ca2b2ea435590de30cdb5b263ebc9e6ee87fc46d
Dmitry7456 <sir.dyf@gmail.com>
2018-12-30 14:28:13 +0500

    add static res

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.get('/', function (req, res) {
+
+app.get('/test', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
+});
+app.get('/', function (req, res) {
+ res.sendFile(__dirname + '/index.html');
 });
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
